614869093e2ca6d7af197d4201148dfd4d6b7661
Gaoh19 <aburahman918@gmail.com>
2023-10-02 15:02:57 +0530

    change asap

--- src/main/java/com/mycompany/attendancetracker/qr/QRCodeUtil.java
+++ src/main/java/com/mycompany/attendancetracker/qr/QRCodeUtil.java
@@ -5,23 +5,38 @@
 package com.mycompany.attendancetracker.qr;
 
 /**
- *
- * @author Abdur
+ * QRCodeUtil provides utility methods for generating and decoding QR codes.
  */
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import javax.imageio.ImageIO;
 
 public class QRCodeUtil {
 
-    // Generate a QR code with the given content and save it to a file
-    public static void generateQRCode(String content, int width, int height, String filePath) {
-        QRCodeGenerator.generateQRCode(content, width, height, filePath);
-    }
+    public static void generateQRCode(String data, int width, int height, String filePath) {
+        try {
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, width, height);
+            BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
+            File qrCodeFile = new File(filePath);
 
-    // Decode a QR code and return the content
-    public static String decodeQRCode(String filePath) {
-        return QRCodeScanner.scanQRCode(filePath);
+            try {
+                if (qrCodeFile.exists() || qrCodeFile.createNewFile()) {
+                    MatrixToImageWriter.writeToPath(bitMatrix, "PNG", qrCodeFile.toPath());
+                    System.out.println("QR code saved to: " + filePath);
+                } else {
+                    System.err.println("Failed to create QR code file.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
 }
