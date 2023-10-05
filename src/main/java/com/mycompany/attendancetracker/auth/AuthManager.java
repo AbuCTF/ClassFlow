f88dbdeae3c5a758acc5c28880aaaf84dd4be926
Gaoh19 <aburahman918@gmail.com>
2023-10-05 13:38:02 +0530

    untested token manager

--- src/main/java/com/mycompany/attendancetracker/auth/AuthManager.java
+++ src/main/java/com/mycompany/attendancetracker/auth/AuthManager.java
@@ -9,5 +9,23 @@ package com.mycompany.attendancetracker.auth;
  * @author USER
  */
 public class AuthManager {
+    private static String authToken;
+    private static boolean loggedIn = false;
+
+    public static void setAuthToken(String token) {
+        authToken = token;
+    }
+
+    public static String getAuthToken() {
+        return authToken;
+    }
+
+    public static void setLoggedIn(boolean status) {
+        loggedIn = status;
+    }
+
+    public static boolean isLoggedIn() {
+        return loggedIn;
+    }
     
 }
