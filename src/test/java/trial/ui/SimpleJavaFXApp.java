777c22e8118ae49d6d0c5be880d7b4d9fc8cf8c9
Gaoh19 <aburahman918@gmail.com>
2023-10-16 23:11:34 +0530

    Time checkup #07.2.1

--- src/test/java/trial/ui/SimpleJavaFXApp.java
+++ src/test/java/trial/ui/SimpleJavaFXApp.java
@@ -11,33 +11,18 @@ package trial.ui;
  */
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-public class SimpleJavaFXApp extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        // Create a label with the text "Hello, JavaFX!"
-        Label label = new Label("Hello, JavaFX!");
-
-        // Create a layout pane and add the label to it
-        StackPane root = new StackPane();
-        root.getChildren().add(label);
-
-        // Create a scene with the layout and set it as the scene for the primary stage
-        Scene scene = new Scene(root, 300, 200);
-        primaryStage.setScene(scene);
-
-        // Set the title of the window
-        primaryStage.setTitle("Simple JavaFX Application");
-
-        // Show the window
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
+ 
+public class SimpleJavaFXApp extends Application 
+{
+    public static void main(String[] args) 
+    {
         launch(args);
+    }
+ 
+    public void start(Stage theStage) 
+    {
+        theStage.setTitle("Hello, World!");
+        theStage.show();
     }
 }
